2e7ea1c07d1e1f27a50fa5051786add528ec1699
Krishna Mohan Athota <krishnamohan.athota@gmail.com>
2023-07-23 19:41:07 +0530

    chatbot-openai project is moved to open-ai folder

--- chatbot-openai/src/Index.js
+++
@@ -1,9 +0,0 @@
-import ReactDOM from "react-dom/client";
-
-import App from "./App.js";
-
-const MainLayOut = () => {
-  return <App />;
-};
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<MainLayOut />);
--- chatbot-openai/src/env.js
+++
@@ -1,2 +0,0 @@
-//Create / Get your API Key : https://platform.openai.com/account/api-keys
-export const API_KEY = "xxx";
